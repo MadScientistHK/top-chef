0a7fd75c4490e82ae6ffe0d0378d23601678e88c
MadScientistHK <jeremy.martin12@hotmail.fr>
2018-03-03 09:16:34 +0100

    suite

--- scrap.js
+++ scrap.js
@@ -2,7 +2,7 @@ const cheerio = require('cheerio');
 const fetch = require('node-fetch');
 const fs = require('fs');
 
-// parameters to fetch the url
+// parameters url
 const fetchParameters = { method: 'GET',
 headers: {},
 follow: 20,
@@ -11,4 +11,21 @@ compress: true,
 size: 0,
 body: null,
 agent: null
+}
+
+//get links of each resaurants
+async function getRestaurantLinksFrom(url)
+{
+  const resp = await fetch(url, fetchParameters);
+  const html = await resp.text();
+  const $ = await cheerio.load(html);
+  const aTag = $("a.poi-card-link");
+  //filter to get only link
+  const filteraTag = aTag.filter(d => aTag[d].name == 'a');
+  const links = [];
+  for (let i = 0; i < filteraTag.length; i++)
+  {
+    links.push("https://restaurant.michelin.fr" + filteraTag[i].attribs.href)
+  }
+  return links;
 }
